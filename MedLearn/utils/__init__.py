892ba41e7baf44a40c74a6fe4ce84dab787a1818
shenwanxiang <shenwanxiang@megvii.com>
2018-12-20 14:34:27 +0800

    add log and run cmd tools

--- MedLearn/utils/__init__.py
+++ MedLearn/utils/__init__.py
@@ -6,4 +6,8 @@ Created on Sat Oct 13 17:22:13 2018
 @author: charleshen
 """
 
-__all__ = ["modelbase", "pandastool"]
+__all__ = ["modelbase", "pandastool, runtools, logtools"]
+
+
+from .logtools import pbar, print_info, print_error, print_warn, print_debug
+from .runtools import RunCmd, MultiProcessRun
